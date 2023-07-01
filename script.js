9ae9b2506fe3cf247907a07c4a0729b13e60f4c9
Navveen K K <navveenkk@gmail.com>
2023-07-01 14:00:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// window.addEventListener('load', function homePage() {
+//     document.getElementById('homepage').click();
+//   });
+
+//   function showPage(page, links) {
+//     var i, mainContent, bgContent, idpage, mainlinks;
+//     mainContent = document.getElementsByClassName('main-content');
+//       for (i = 0; i < mainContent.length; i++) {
+//       mainContent[i].style.display = "none";
+//       }
+
+//     mainlinks = document.getElementsByClassName('navigation-menu-item');
+//       for (i = 0; i < mainlinks.length; i++) {
+//         mainlinks[i].classList.remove('active');
+//       }
+      
+//     links.parentElement.classList.add('active');
+//     idpage = document.getElementById(page);
+//     idpage.style.display = "block";
+//   }
+
+//   function switchLight() {
+//     var bodySwitchMode = document.getElementById('mainpage');
+//     bodySwitchMode.classList.toggle('night-mode');
+
+//   }
